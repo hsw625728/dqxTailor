bfc94ee7684f586d49ea33c0a3390cf8f5cd9874
PangXieOffice <hsw625728@163.com>
2017-01-06 16:20:31 +0800

    工程环境搭建

---
+++ dqxTailor/dqxTailor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dqxTailor
+//
+//  Created by HANSHAOWEN on 17/1/6.
+//  Copyright © 2017年 HANSHAOWEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dqxTailor/dqxTailor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dqxTailor
+//
+//  Created by HANSHAOWEN on 17/1/6.
+//  Copyright © 2017年 HANSHAOWEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
